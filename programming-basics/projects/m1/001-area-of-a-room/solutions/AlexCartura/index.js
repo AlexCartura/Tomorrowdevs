2b73433696363609ace9c4a72bc5ade05a39908f
AlexCartura <thatalexbaby@gmail.com>
2022-09-06 12:16:46 +0200

    solution/area-of-a-room

--- programming-basics/projects/m1/001-area-of-a-room/solutions/AlexCartura/index.js
+++ programming-basics/projects/m1/001-area-of-a-room/solutions/AlexCartura/index.js
@@ -1,1 +1,5 @@
-// bla bla bla bla bla bla
+let width = parseFloat (prompt("insert the widht in meters", "0"));
+let lenght = parseFloat (prompt("insert the lenght in meters", "0"));
+
+let totalarea = width * lenght;
+alert("The area is " + totalarea + " meters");
