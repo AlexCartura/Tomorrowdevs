2c4a84274f3ddd4f2a85601581996f44567e1235
AlexCartura <thatalexbaby@gmail.com>
2022-09-22 21:53:46 +0200

    solution/UNITS OF TIME (AGAIN)

--- programming-basics/projects/m1/005-units-of-time-again/solutions/AlexCartura/solution.js
+++ programming-basics/projects/m1/005-units-of-time-again/solutions/AlexCartura/solution.js
@@ -5,15 +5,28 @@ const second = 60;
 
 const seconds = prompt("How many seconds do you want to convert? ")
 
-const day_ris = parseInt(seconds / days);
+let day_ris = parseInt(seconds / days);
 const day_mod = seconds % days;
 
-const hours_ris = parseInt(day_mod/hours);
+let hours_ris = parseInt(day_mod/hours);
 const hours_mod = day_mod%hours;
 
-const minutes_ris = parseInt(hours_mod/minutes);
+let minutes_ris = parseInt(hours_mod/minutes);
 const minutes_mod = hours_mod%minutes;
 
-const second_ris = parseInt(minutes_mod/second)
+let second_ris = parseInt(minutes_mod/second);
+
+if (day_ris < 10) {
+    day_ris = "0"+ day_ris
+}
+if (hours_ris < 10) {
+    hours_ris = "0"+ hours_ris
+}
+if (minutes_ris < 10) {
+    minutes_ris = "0"+ minutes_ris
+}
+if (second_ris < 10) {
+    second_ris = "0"+ second_ris
+}
 
 alert("the time is " + day_ris +":"+hours_ris+":"+minutes_ris+":"+second_ris)
