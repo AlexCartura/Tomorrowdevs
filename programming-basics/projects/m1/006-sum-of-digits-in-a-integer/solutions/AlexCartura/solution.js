ac2d7446b8097b03df51a09b3be9f14e57b2f609
AlexCartura <thatalexbaby@gmail.com>
2022-09-25 09:23:28 +0200

    solution/sum-of-digits-in-a-integer

---
+++ programming-basics/projects/m1/006-sum-of-digits-in-a-integer/solutions/AlexCartura/solution.js
@@ -0,0 +1,10 @@
+const str = prompt("Insert your nnumber here !");
+
+const firstNumber = str[0];
+const secondNumber = str[1];
+const thirdNumber = str[2];
+const fourthNumber = str[3];
+
+const total = (firstNumber + secondNumber + thirdNumber + fourthNumber);
+
+alert(`The sum of your number digits is: ${+firstNumber + +secondNumber + +thirdNumber + +fourthNumber}`)
