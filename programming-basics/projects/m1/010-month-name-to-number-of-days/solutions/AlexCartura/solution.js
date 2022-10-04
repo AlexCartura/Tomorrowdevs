7a08b64f4c31fe77b0650f4817b98de825e9416c
AlexCartura <thatalexbaby@gmail.com>
2022-10-04 22:52:09 +0200

    month-name-to-number-of-days final

--- programming-basics/projects/m1/010-month-name-to-number-of-days/solutions/AlexCartura/solution.js
+++ programming-basics/projects/m1/010-month-name-to-number-of-days/solutions/AlexCartura/solution.js
@@ -2,15 +2,14 @@ let date = new Date();
 let year = date.getFullYear();
 
 let month = prompt("insert a month here!")
-month = month.toLocaleLowerCase;
+month = month.toLocaleLowerCase();
 
 switch(month) {
-
     case "november":
     case "april":
     case "june":
     case "september":
-    alert("30 days");
+        alert("30 days");
     break;
     
     case "january":
@@ -20,18 +19,20 @@ switch(month) {
     case "august":
     case "october":
     case "december":
-    alert("31 days");
+        alert("31 days");
     break;
 
     case "february":
-        if (year%400==0) {
-            alert("29 days")
-        } else if (year%4 == 0 && year % 100 != 0){
-            alert("28 days")
-        }
-    break
+    if (year%400==0){
+    alert ("29 days");
+    } else if (year % 4 == 0 && year % 100 != 0){
+    alert ("29 days");
+    } else {
+    alert("28 days");
+    }
+    break;
 
     default:
-        alert("this is not a month!")
-
+        alert("this is not a month")
+   
 }
